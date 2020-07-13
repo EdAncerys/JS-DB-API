1cebf6578f21e69faaf6bb99c1e627a622f2698c
Ed <lookatemail@gmail.com>
2020-07-13 13:21:14 +0100

    Able to connect and get info from DB

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const mysql = require('mysql');
+const express = require('express');
+
+const app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
 
 const mysqlConnection = mysql.createConnection({
   host: 'localhost',
@@ -13,4 +19,17 @@ mysqlConnection.connect((error) => {
     console.log(
       'DB connection failed \n Error : ' + JSON.stringify(error, undefined, 2)
     );
+});
+
+app.listen(3000, () =>
+  console.log('Express server is running at port no: 3000 ')
+);
+
+// CRUD actions
+// Read from DB
+app.get('/users', (res, req) => {
+  mysqlConnection.query('SELECT * FROM users', (error, rows, fields) => {
+    if (!error) console.log(rows);
+    else console.log(error);
+  });
 });
